167a75a6f5434ab80f685a087aad02059cd25c7e
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-02-12 18:00:13 -0300

    feat: add project settings

--- src/main/java/br/devrafaelsoares/SpringBootAuth/SpringBootAuthApplication.java
+++ src/main/java/br/devrafaelsoares/SpringBootAuth/SpringBootAuthApplication.java
@@ -2,8 +2,10 @@ package br.devrafaelsoares.SpringBootAuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync(proxyTargetClass = true)
 public class SpringBootAuthApplication {
 
 	public static void main(String[] args) {
